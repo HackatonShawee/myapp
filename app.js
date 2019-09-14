33b1a6bac49fa18bfbd85cc8f9e6f3021a5d8cd0
hackatonshawee <hackatonshawee@gmail.com>
2019-09-14 18:20:53 -0300

    Second commit:Marconi

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.send('Hello World!');
+    res.send('Hello World Marconi!');
 });
 
 app.listen(port,() =>{
-    console.log ('Servidor online na porta ${port}')
+    console.log (`Servidor online na porta ${port}`)
 });
